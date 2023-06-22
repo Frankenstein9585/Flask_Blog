f4a80628282b96b6ccff46fec879bf3324d0f8a8
Donald Peters <donny9585@gmail.com>
2023-06-22 13:55:19 +0100

    Added functionality to update account information

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,6 +1,6 @@
 from flaskblog.models import User, Post
 from flask import render_template, url_for, flash, redirect, request
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import UpdateAccountForm, LoginForm, RegistrationForm
 from flaskblog import app, bcrypt, db
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -72,5 +72,6 @@ def logout():
 @app.route('/account')
 @login_required
 def account():
+    form = UpdateAccountForm()
     image_file = url_for('static', filename=current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
