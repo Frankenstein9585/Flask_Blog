ba0f843c8895452af5843c00a37522cba6209243
Frankenstein9585 <donny9585@gmail.com>
2023-06-23 20:15:56 +0100

    Added profile picture functionality

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,3 +1,6 @@
+import secrets
+import os
+from PIL import Image
 from flaskblog.models import User, Post
 from flask import render_template, url_for, flash, redirect, request
 from flaskblog.forms import UpdateAccountForm, LoginForm, RegistrationForm
@@ -69,9 +72,36 @@ def logout():
     return redirect(url_for('hello'))
 
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, file_extension = os.path.splitext(form_picture.filename)
+    picture_filename = random_hex + file_extension
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_filename)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+
+    i.save(picture_path)
+    return picture_filename
+
+
+@app.route('/account', methods=['POST', 'GET'])
 @login_required
 def account():
     form = UpdateAccountForm()
-    image_file = url_for('static', filename=current_user.image_file)
+
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/{}'.format(current_user.image_file))
     return render_template('account.html', title='Account', image_file=image_file, form=form)
